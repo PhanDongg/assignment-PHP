9ce33b7b3fe7665e2a2521023fa5ca717a7ffbb5
LAPTOP-STJL294T\admin <phandong.hungthinhme@gmail.com>
2023-06-17 12:30:07 +0700

    cập nhật phần edit và delete

--- wp-content/plugins/owt-table-list.php
+++ wp-content/plugins/owt-table-list.php
@@ -1,5 +1,7 @@
 <?php
 
+//tinhs nang sort chuwa duowc
+//searcho chuwa dc. ddeen phut thu 11
 require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 
 class OWTTableList extends WP_List_Table {
@@ -11,7 +13,21 @@ class OWTTableList extends WP_List_Table {
         $orderby = isset($_GET['orderby']) ? trim($_GET['orderby']) : "";
         $order = isset($_GET['order']) ? trim($_GET['order']) : "";
 
-        $this->items = $this->wp_list_table_data($orderby, $order);
+        $datas = $this->wp_list_table_data($orderby, $order);
+
+        //phân trang
+        $per_page = 10;
+        $current_page = $this->get_pagenum();
+        $total_items = count($datas);
+
+        $this->set_pagination_args(array(
+            "total_items" => $total_items,
+            "per_page" => $per_page
+        ));
+        $this->items = array_slice($datas, (($current_page - 1) * $per_page), $per_page);
+
+        //1-1 => 0 * 3 = 0 , 1 , 2
+        //-1 => 1*3 = 3, 4, 5
 
         $columns = $this->get_columns();
 
@@ -26,22 +42,37 @@ class OWTTableList extends WP_List_Table {
 
     public function wp_list_table_data($orderby = '', $order = '') {
 
-        $all_posts = get_posts(array (
-            "post_type"=> "post",
-            "post_status"=> "publish"
-        ));
-        
-//        print_r($all_post);
+        global $wpdb;
+        if ($orderby == "title" && $order == "desc") {
+
+            $all_posts = $wpdb->get_results(
+                    "SELECT * from" . $wpdb->posts . " WHERE post_type = 'post' AND post_status = 'publish' ORDER BY post_title DESC"
+            );
+        } else {
+            $all_posts = $wpdb->get_results(
+                    "SELECT *from " . $wpdb->posts . " WHERE post_type = 'post' AND post_status = 'publish' ORDER BY id desc"
+            );
+//            $all_posts = get_posts(array(
+//                "post_type" => "post",
+//                "post_status" => "publish"
+//            ));
+        }
+
+//        print_r($all_posts);
+
         $posts_array = array();
-        
+
         if (count($all_posts) > 0) {
-            
+
             foreach ($all_posts as $index => $post) {
                 $posts_array[] = array(
-                    "id"=>$post->ID,
-                    "title"=>$post->post_title,
-                    "content"=>$post->post_content,
-                    "slug"=>$post->post_name
+                    "id" => $post->ID,
+                    "title" => $post->post_title,
+                    "content" => $post->post_content,
+//                    "slug"=>$post->post_name,
+                    "post_date" => $post->post_date,
+                    "post_date_gmt" => $post->post_date_gmt,
+                    "post_status" => $post->post_status
                 );
             }
         }
@@ -50,14 +81,17 @@ class OWTTableList extends WP_List_Table {
 
     public function get_hidden_columns() {
 
-        return array("id"); //tham số là cột cần ẩn
+        return array("post_status"); //tham số là cột cần ẩn
     }
 
     public function get_sortable_columns() {
 
         return array(
-            "name" => array("name", true), //true là có sắp xếp theo ESC hoac DESC
-//            "email" => array("email", false)
+            "title" => array("title", true), //true là có sắp xếp theo ESC hoac DESC
+            "content" => array("content", true),
+            "post_date" => array("post_date", true),
+            "post_date_gmt" => array("post_date_gmt", true),
+            "post_status" => array("post_status", true),
         );
     }
 
@@ -66,9 +100,12 @@ class OWTTableList extends WP_List_Table {
 
         $columns = array(
             "id" => "ID",
-            "title" => "Title",
-            "content" => "Content",
-            "slug" =>  "Post Slug"
+            "title" => "Tiêu Đề",
+            "content" => "Nội Dung",
+//            "slug" =>  "Post Slug",
+            "post_date" => "Ngày Bắt Đầu",
+            "post_date_gmt" => "Ngày Kết Thúc",
+            "post_status" => "Trạng Thái"
         );
 
         return $columns;
@@ -82,11 +119,24 @@ class OWTTableList extends WP_List_Table {
             case 'id':
             case 'title':
             case 'content':
-            case 'slug':
+//            case 'slug':
+            case 'post_date':
+            case 'post_date_gmt':
+            case 'post_status':
                 return $item[$column_name];
             default:
                 return "no value";
         }
+    }
+    
+    public function column_title($item) {
+        
+        $action = array(
+            "edit"=>"<a href='?page=".$_GET['page']."&action=owt-edit&post_id=".$item['id']."'>Edit</a>",
+//            "edit"=>"<a href='?page="'>EIDT</a>"
+            "delete"=>"<a href='?page=".$_GET['page']."&action=owt-delete&post_id=".$item['id']."'>Delete</a>"
+        );
+        return sprintf('%1$s %2$s', $item['title'], $this->row_actions($action));
     }
 
 }
@@ -97,7 +147,15 @@ function owt_show_data_list_table() {
 
     $owt_table->prepare_items();
 
-    echo '<h3>This is List Table</h3>';
+    echo '<h3>QUẢN LÍ THÔNG BÁO</h3>';
+
+//    echo "<form method='post' name = 'frm_search_post' action='".$_SERVER['PHP_SELF']."?page=owt-list-table'>";
+//    $owt_table -> search_box("Search Post(s)", "search_post_id");
+//    echo "</form>";
+
+    echo "<form method='post' name='frm_search_post' action='" . $_SERVER['PHP_SELF'] . "?page=owt-list-table'>";
+    $owt_table->search_box("Search Post(s)", "search_post_id");
+    echo "</form>";
 
     $owt_table->display();
 }
--- wp-content/plugins/wp-owt-list-table.php
+++ wp-content/plugins/wp-owt-list-table.php
@@ -1,24 +1,46 @@
 <?php
 
-/* 
-    Plugin Name: OWT LIST TABLE
-    Description: This is list table
+/*
+  Plugin Name: Quản Lí Thông Báo
+  Plugin URI: kenh14.vn
+  Description: assginment
+  Version: 1.0
+  Author: David Dong
+  Author URI:  facebook.com/phan.dong.144
  */
 
 add_action("admin_menu", "wpl_Out_list_table_menu");
 
 function wpl_Out_list_table_menu() {
-   
-    add_menu_page("OWT List Table", "OWT List Table", "manage_options", "owt-list-table", "wpl_owt_list_table_fn");
+
+    add_menu_page("Quản Lí Thông Báo", "Quản Lí Thông Báo", "manage_options", "owt-list-table", "wpl_owt_list_table_fn");
 }
 
 function wpl_owt_list_table_fn() {
-    
-    ob_start();
-    
-//    include_once plugin_dir_path(__FILE__) . 'owt-table-list.php';
-    $template = ob_get_contents();
-    
-    ob_end_clean();
-    echo $template;
+
+    $action = isset($_GET['action']) ? trim($_GET['action']) : "";
+
+    if ($action == "owt-edit") {
+
+        $post_id = isset($_GET['post_id']) ? intval($_GET['post_id']) : "";
+        
+        ob_start();
+
+        include_once plugin_dir_path(__FILE__) . 'owt-edit-fn.php';
+
+        $template = ob_get_contents();
+
+        ob_end_clean();
+        echo $template;
+    } else {
+
+        ob_start();
+
+        include_once plugin_dir_path(__FILE__) . 'owt-table-list.php';
+
+        $template = ob_get_contents();
+
+        ob_end_clean();
+        echo $template;
+    }
 }
